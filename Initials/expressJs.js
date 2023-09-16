abe433a350286b606fdb59dfdc15717a586e2303
Vinay Pandey <51539107+vinay02022@users.noreply.github.com>
2023-09-16 17:16:49 +0530

    Update expressJs.js
    Server Live

--- Initials/expressJs.js
+++ Initials/expressJs.js
@@ -1,1 +1,16 @@
+const express=require('express')
+const server=express()
+server.use((req,res,next)=>{
+    console.log(1)
+    next();
+})
+server.use((req,res,next)=>{
+    console.log(1)
+    res.send('Hello i am Created Brother')
+    
+})
 
+
+server.listen(4000,()=>{
+    console.log("server Started.....");
+})
