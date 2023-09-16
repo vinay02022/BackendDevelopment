eb004ed6a6b26f8072a56d5523849d0771fafada
Vinay Pandey <51539107+vinay02022@users.noreply.github.com>
2023-09-16 18:03:11 +0530

    Update expressJs.js
    FormData

--- Initials/expressJs.js
+++ Initials/expressJs.js
@@ -1,16 +1,38 @@
-const express=require('express')
-const server=express()
-server.use((req,res,next)=>{
-    console.log(1)
-    next();
-})
-server.use((req,res,next)=>{
-    console.log(1)
-    res.send('Hello i am Created Brother')
+// const express=require('express')
+// const server=express()
+// server.use((req,res,next)=>{
+//     console.log(1)
+//     next();
+// })
+// server.use((req,res,next)=>{
+//     console.log(1)
+//     res.send('Hello i am Created Brother')
     
-})
+// })
 
 
-server.listen(4000,()=>{
-    console.log("server Started.....");
-})
+// server.listen(4000,()=>{
+//     console.log("server Started.....");
+// })
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen(3000);
+
